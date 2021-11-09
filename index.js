a6de1b070da8ee964eaa88f64655e7d7237ad65a
rostamali <rostamsardar446@gmail.com>
2021-11-09 13:27:41 +0600

    add the code

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
+// cors connect
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
 
 // mongoDB connection
 
-const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.urhyn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // interaction with mongoDB
 
-async function runServer() {
+async function run() {
     try{
         await client.connect();
         const database = client.db('hotOnion');
@@ -29,7 +33,7 @@ async function runServer() {
         // await client.close();
     }
 }
-runServer().catch(console.dir);
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
